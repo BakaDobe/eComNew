576ca0c38affe39042049b6892eb3723bc7edfbd
BakaDobe <anthoniuspowellus@gmail.com>
2022-11-03 10:39:21 -0400

    Added 2fa setup link

--- app/views/User/account.php
+++ app/views/User/account.php
@@ -1,5 +1,5 @@
 <html>
-<head><title>Login</title>
+<head><title>User account</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </head>
@@ -19,6 +19,8 @@
 		</div>
 	<?php }
 ?>
+
+<a href="/User/setup2fa">Set up 2-factor authentication</a>
 
 <h1>Modify your password</h1>
 <form action='' method='post'>
